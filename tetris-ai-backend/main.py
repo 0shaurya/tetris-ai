0b1d52c27d715f69be9c8f9567589172059fd3bd
Shaurya Sehgal <56615472+0shaurya@users.noreply.github.com>
2025-05-05 16:54:56 -0500

    Update main.py

--- tetris-ai-backend/main.py
+++ tetris-ai-backend/main.py
@@ -169,5 +169,7 @@ def move():
         print(f"Error: {e}")
         return jsonify({'move':0,'rotate':0,'drop':True,'soft_drop':False})
 
+import os
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
